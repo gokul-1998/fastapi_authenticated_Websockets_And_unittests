bd172353c1dc901a4c165257beb340bf845d3404
gokul-1998 <gokulakrishnanm1998@gmail.com>
2023-12-12 12:47:49 +0530

    added structured test cases

--- application/main.py
+++ application/main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from fastapi.testclient import TestClient
 from fastapi.websockets import WebSocket
 
 app = FastAPI()
@@ -17,15 +16,3 @@ async def websocket(websocket: WebSocket):
     await websocket.close()
 
 
-def test_read_main():
-    client = TestClient(app)
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"msg": "Hello World"}
-
-
-def test_websocket():
-    client = TestClient(app)
-    with client.websocket_connect("/ws") as websocket:
-        data = websocket.receive_json()
-        assert data == {"msg": "Hello WebSocket"}
