df321ae43f9c64f33282e5c33982203c0ce06119
gokul-1998 <gokulakrishnanm1998@gmail.com>
2023-12-12 12:37:32 +0530

    added structured test cases

---
+++ tests/websocket_test.py
@@ -0,0 +1,10 @@
+
+
+from fastapi.testclient import TestClient
+from application.main import app
+
+def test_websocket():
+    client = TestClient(app)
+    with client.websocket_connect("/ws") as websocket:
+        data = websocket.receive_json()
+        assert data == {"msg": "Hello WebSocket"}
